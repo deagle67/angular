f6471daeb05e812db27b0d579cebe93497b0280e
deagle67 <Neko@TABLET-AQ7D2IAM>
2017-11-17 17:15:11 +0100

    migration ok

--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Vega.Models;
 
 namespace Vega.Persistence
 {
@@ -8,5 +9,7 @@ namespace Vega.Persistence
         {
             
         }
+
+        public DbSet<Make> Makes { get; set; }
     }
 }
